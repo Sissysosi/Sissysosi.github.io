d563b71ac108d398187f27106cb65d1127a2a42f
NurulAkmilina <20ftt1457@student.pb.edu.bn>
2022-04-14 23:30:07 +0800

    succesfully import script to index.html

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,43 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+<div class="flex items-center justify-center mt-5">
+<div class="bg-blue-900 w-1/8 p-10 h-auto">
+<div class="flex items-center justify-center mb-10">
+		 <img src="./assets/lina.jpeg" class="object-contain h-48 w-96 rounded-lg " alt="">
+		</div>
+ <div class="bg-orange-900 w-1/8 p-10 h-auto rounded-lg">
+     <h1 class="text-gray-400 uppercase tracking-widest text-lg font-bold">Personal Details</h1>
+    <hr class="my-5">
+    <h1 class="text-gray-400 text-sm">SITI NURUL AKMILINA BINTI SAMRAH  </h1>
+    <h1 class="text-gray-400 text-sm">+673 8697114</h1>
+		<h1 class="text-gray-400 text-sm">nurulakmilina@gmail.com</h1>
+    	<h1 class="text-gray-400 text-sm">Student ID : 20ftt1457</h1>
+      	<h1 class="text-gray-400 text-sm">Dwdt06</h1>
+
+    <hr class="my-5">
+   </div>
+   <div class="bg-green-900 w-1/8 p-10 h-auto rounded-lg">
+     <h1 class="text-gray-400 uppercase tracking-widest text-lg font-bold">Hobbies </h1>
+    <hr class="my-5">
+    <h1 class="text-gray-400 text-sm">Swimming </h1>
+    <h1 class="text-gray-400 text-sm">Netball</h1>
+    <hr class="my-5">
+   
+</div>
+   <div class="bg-purple-900 w-1/8 p-10 h-auto rounded-lg">
+     <h1 class="text-gray-400 uppercase tracking-widest text-lg font-bold">Education </h1>
+    <hr class="my-5">
+    <h1 class="text-gray-400 text-sm">7 O'LEVEL </h1>
+    <h1 class="text-gray-400 text-sm">Diploma in Web development</h1>
+    <hr class="my-5">
+   
+</div>
+</div>
+
+</div>
+
+  
 </template>
 
 <style>
